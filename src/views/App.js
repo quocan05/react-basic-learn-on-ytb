0dfb1b48c4da15a7fb4b1ec855cef9bfa4b349ec
Quoc An <dangquocanf5@gmail.com>
2023-11-06 23:50:34 +0700

    ok create project, know scss, little export, create class component

--- src/views/App.js
+++ src/views/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import MyExample from "./example/MyExample";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World by React-QuocAn
-        </p>
+        <p>Hello World by React-QuocAn</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,16 +16,21 @@ function App() {
         >
           Learn React
         </a>
-        <MyButton />
+        {/* <MyButton /> */}
+        {/* <MyProfile /> */}
+        {/* <MyExample /> */}
+        <MyExample></MyExample>
       </header>
     </div>
   );
 }
 
 function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
+  return <button>I'm a button</button>;
+}
+
+function MyProfile() {
+  return <div> this is my profile </div>;
 }
 
 export default App;
---
+++ src/views/example/MyExample.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const myButton = <button>button of MyExample</button>;
+const myDiv = <div>đầu buồi </div>;
+class MyExample extends React.Component {
+  render() {
+    return myDiv;
+  }
+}
+
+export default MyExample;

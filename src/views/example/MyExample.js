212c1b96f9b404cd3c2ec651a6632ed9f4ed3374
Quoc An <dangquocanf5@gmail.com>
2023-11-07 22:40:25 +0700

    know state, install dev tools

--- src/views/example/MyExample.js
+++ src/views/example/MyExample.js
@@ -2,9 +2,33 @@ import React from "react";
 
 const myButton = <button>button of MyExample</button>;
 const myDiv = <div>đầu buồi </div>;
+let name = "Quocan";
 class MyExample extends React.Component {
+  state = {
+    name: "Quoc An",
+    age: 21,
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
   render() {
-    return myDiv;
+    return (
+      <>
+        <div>
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChange(event)}
+          />
+          hello my name is {this.state.name}
+        </div>
+        <div>my age : {this.state["age"]}</div>
+      </>
+    );
   }
 }
 

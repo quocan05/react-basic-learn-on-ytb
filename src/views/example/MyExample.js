ce8c81ab6bf014add2865a64a1d503c166ea6a99
Quoc An <dangquocanf5@gmail.com>
2023-11-08 13:58:51 +0700

    ok setstate, re-render, form, eventhandle

--- src/views/example/MyExample.js
+++ src/views/example/MyExample.js
@@ -5,28 +5,62 @@ const myDiv = <div>đầu buồi </div>;
 let name = "Quocan";
 class MyExample extends React.Component {
   state = {
-    name: "Quoc An",
-    age: 21,
+    username: "",
+    password: "",
   };
 
   handleOnChange = (event) => {
+    //console.log(name, " event target: ", event.target, " event: ", event);
     this.setState({
-      name: event.target.value,
+      username: event.target.value,
+      password: event.target.value,
     });
+  };
+
+  handleClick = () => {
+    alert("hello world");
+  };
+
+  handleSubmitForm = (event) => {
+    event.preventDefault();
+    console.log(this.state);
   };
 
   render() {
     return (
       <>
-        <div>
+        {/* <div>
           <input
             value={this.state.name}
             type="text"
             onChange={(event) => this.handleOnChange(event)}
           />
           hello my name is {this.state.name}
-        </div>
-        <div>my age : {this.state["age"]}</div>
+        </div> */}
+
+        <form>
+          <label htmlFor="username">username: </label>
+          <input
+            id="username"
+            type="text"
+            onChange={(event) => this.handleOnChange(event)}
+          />{" "}
+          <br />
+          <label htmlFor="password">password: </label>
+          <input
+            id="password"
+            type="text"
+            onChange={(event) => this.handleOnChange(event)}
+          />{" "}
+          <br />
+          <input
+            type="submit"
+            onClick={(event) => this.handleSubmitForm(event)}
+          />
+        </form>
+
+        <div>my age : {this.state["password"]}</div>
+        <button onClick={() => this.handleClick()}>Click me</button>
       </>
     );
   }
